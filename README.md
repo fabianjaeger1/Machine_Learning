5dfb7a155eb7b194d3c765a48a1beed31d8fa67f
fabianjaeger1 <fabian_jaeger@outlook.com>
2024-02-27 23:02:25 +0100

    add readme

---
+++ README.md
@@ -0,0 +1,17 @@
+# Machine Learning
+
+This repository contains code projects and write-ups about topics in statistics, probability and machine learning broadly.
+
+
+### ToDo
+- [ ] Monte-Carlo Simulation
+- [ ] Hypothesis testing
+- [ ] Ensemble learning
+
+
+#### Resources 
+---- 
+- **Probabilistic Machine Learning: An Introduction - Murphy**
+- Practical Statistics for Data Scientists - Bruce
+- Essential Math for Data Science - Nield
+- A modern introduction to Probability and Statistics - Dekking
--- document_classifier/docreader.py
+++ document_classifier/docreader.py
@@ -9,7 +9,17 @@ from constants import (
 
 def load_documents(src_dir: str) -> list[Document]:
     '''Loads all files from the source directory as well as any nested directories'''
+    paths = []
     
-    return 
+    
+    # Walk the directories and append any documents that match the DOCUMENT_MAP constant document types
+    for root, _, files in os.walk(src_dir):
+        for file_name in files:
+            print("Importing " + file_name)
+            file_ext = os.path.splitext(file_name)[1]
+            src_file_path = os.path.join(root, file_name)
+            if file_ext in DOCUMENT_MAP.keys():
+                paths.append(src_file_path)
+    
     
     
